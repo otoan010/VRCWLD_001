427442c331f9c6e01e1a75359068eea5a974a526
otoan010 <otoan010.vlab@gmail.com>
2020-07-27 18:04:44 +0900

    的あて3Dゲームのチュートリアルを開始 : 的の作成とボールを飛ばす機能を作成

---
+++ VRCWLD_001_Prot/Assets/otoan010/Scripts/ShotBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBall : MonoBehaviour
+{
+
+    [SerializeField]
+    private float throwX = 0;
+     [SerializeField]
+    private float throwY = 300;
+     [SerializeField]
+    private float throwZ = 1500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Throw(new Vector3(0, throwY, throwZ));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Throw(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+}
