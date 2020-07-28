4795bcc0c9183c3efc793d81eeced6c2c24621e6
otoan010 <otoan010.vlab@gmail.com>
2020-07-28 17:54:46 +0900

    的あて3Dゲームのチュートリアル進行中 : 機能を作成した
    的を左右に動かす機能と的にボールが当たるとパーティクルが発生する機能を作成

---
+++ VRCWLD_001_Prot/Assets/otoan010/Scripts/matoTargetMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class matoTargetMover : MonoBehaviour
+{
+    
+    private Vector3 targetpos;
+
+    [SerializeField]
+    private float targetSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * targetSpeed + targetpos.x, targetpos.y, targetpos.z);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<ParticleSystem>().Play();
+    }
+
+
+}
