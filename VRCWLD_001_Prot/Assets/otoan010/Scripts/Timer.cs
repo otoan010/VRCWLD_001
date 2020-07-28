80a61da78d2331415aed8bef86ab9eebe6e8fe00
otoan010 <otoan010.vlab@gmail.com>
2020-07-28 18:35:09 +0900

    的あて3Dゲームのチュートリアル進行中 : 機能を作成した
    制限時間をカウントダウンする機能とスコアをカウントアップする機能を作成

---
+++ VRCWLD_001_Prot/Assets/otoan010/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    
+    public Text timeText;
+    float totalTime = 60;
+    int retime;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+        retime = (int)totalTime;
+        timeText.text = retime.ToString();
+    }
+}
---
+++ VRCWLD_001_Prot/Assets/otoan010/Scripts/matoTargetManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class matoTargetManager : MonoBehaviour
+{
+    
+    private Vector3 targetpos;
+    public Text scoreText;
+    private int score = 0;
+
+    [SerializeField]
+    private float targetSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * targetSpeed + targetpos.x, targetpos.y, targetpos.z);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        score += 10;
+        scoreText.text = string.Format("Score : {0}", score); 
+        
+        GetComponent<ParticleSystem>().Play();
+    }
+
+
+}
