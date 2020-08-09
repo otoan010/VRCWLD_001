3075092516f541db4a690c549c4e7ef13dfa1377
otoan010 <otoan010.vlab@gmail.com>
2020-08-09 17:49:57 +0900

    Testシーンを作成 : 機能を作成
    matoTarget.prefabがランダムで生成されプレイヤー方向に向かってくる様にした

---
+++ VRCWLD_001_Prot/Assets/otoan010/Scripts/Test/MatoGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatoGenerator : MonoBehaviour
+{
+   public GameObject MatoPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject enemy = Instantiate(MatoPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VRCWLD_001_Prot/Assets/otoan010/Scripts/Test/MatoMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatoMover : MonoBehaviour
+{
+    
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Time.deltaTime * speed;
+
+        transform.position -= new Vector3(0, 0, z);
+
+        if (transform.position.z < -30)
+        {
+            Init();
+        }
+    }
+
+    void Init()
+    {
+        speed = Random.Range(20f, 60f);
+
+        float x = Random.Range(-1f, 1f);
+        //float x = Random.Range(-3f, 3f);
+
+        float y = Random.Range(0.5f, 3f);
+        
+        float z = 50;
+        transform.position = new Vector3(x, y, z);
+    }
+}
